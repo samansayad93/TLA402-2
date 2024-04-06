d31b585f00fad7ad282b7fe8e65d3a82accf47a4
Kiasoh <kiarashs37@gmail.com>
2024-04-06 15:42:41 +0330

    main cleaned up and models updated

--- main.cpp
+++ main.cpp
@@ -11,37 +11,20 @@ public:
     bool final_state;
     State(string name) : name(name)
     {
-        // State.states["hi"] = nullptr;
         final_state = false;
         states.insert({name , this});
     }
     static void add_neighbor (string &start , char input , string &dest )
     {
         states[start]->add_neighbor(input , states[dest]);
-        // if (input == '')states[dest]->stalkers.insert(states[start]);
     }
 
     vector<State*> get_neighbor(char input)
     {
-        // cout << "asd";
-        // vector <State*> ans;
-        // for(auto haha : neighbors[input])
-        // {
-        //     ans.push_back(haha);
-        // }
-        // return ans;
         return neighbors[input];
     }
     vector<State*> get_sami()
     {
-        // cout << "asd";
-        
-        // vector <State*> ans;
-        // for(auto haha : neighbors['$'])
-        // {
-        //     ans.push_back(haha);
-        // }
-        // return ans;
         return neighbors['$'];
     }
     
@@ -53,27 +36,17 @@ public:
     {
         states[name]->final_state = true;
     }
-    map <char , vector <State*>> neighbors;
-    set<State*> stalkers;
 private:
+    map <char , vector <State*>> neighbors;
 
 };
 
 
 bool NFA(string pattern , State * curr , int i)
 {
-    // cout << curr->name << endl;
-        // cout <<"joke"; 
-    if(pattern.size() == i)
-    {
-        // cout << curr->final_state;
-    return curr->final_state;
-        
-    }
+    if(pattern.size() == i) return curr->final_state;
      
-    vector <State *> neighbor = curr->get_neighbor(pattern[i]);
-
-    for (auto haha : neighbor)
+    for (auto haha : curr->get_neighbor(pattern[i]))
     {
         if (NFA(pattern , haha , i + 1))
             return true;
@@ -119,16 +92,11 @@ int main()
     for(int i = 0 ; i  < n; i ++)
     {
         cin >> a;
-
-        // cout << a;
-        // c= a.substr(a.find_first_of(',') ,a.las )[0];
         c = a[a.find_first_of(',') + 1] ;
         string k = a.substr(0 , a.find_first_of(','))  , kk = a.substr(a.find_last_of(',') + 1);
         State::add_neighbor(k, c ,kk );
     }
-    // cout << states["q3"]->final_state;
     string pattern;
     cin >> pattern;
-    // cout << start->neighbors.size();
     cout << (NFA(pattern , start , 0) ? "Accepted" : "Rejected");
 }
--- models.h
+++ models.h
@@ -1,48 +1,62 @@
 #include <bits/stdc++.h>
-using std::vector;
-using std::map;
-// using std::multimap;
-using std::string;
+using namespace std;
 
+class State;
 
+map<string , State*> states;
 
 class State
 {
 public:
-    static map<string , State*> states;
     string name;
     bool final_state;
     State(string name) : name(name)
     {
         final_state = false;
-        State::states.insert({name , this});
+        states.insert({name , this});
     }
-    static void add_neighbor (string start , char input , string dest )
+    static void add_neighbor (string &start , char input , string &dest )
     {
-        State::states[start]->add_neighbor(input , State::states[dest]);
+        states[start]->add_neighbor(input , states[dest]);
     }
+
     vector<State*> get_neighbor(char input)
     {
-        vector <State*> ans;
-        for(auto haha : neighbors[input])
-        {
-            ans.push_back(haha);
-        }
-        for(auto haha : neighbors['$'])
-        {
-            ans.push_back(haha);
-        }
-        return ans;
+        return neighbors[input];
     }
+    vector<State*> get_sami()
+    {
+        return neighbors['$'];
+    }
+    
     void add_neighbor(char input , State* dest)
     {
         neighbors[input].push_back(dest);
     }
     static void make_finale(string name)
     {
-        State::states[name]->final_state = true;
+        states[name]->final_state = true;
     }
 private:
     map <char , vector <State*>> neighbors;
 
 };
+
+
+bool NFA(string pattern , State * curr , int i)
+{
+    if(pattern.size() == i) return curr->final_state;
+     
+    for (auto haha : curr->get_neighbor(pattern[i]))
+    {
+        if (NFA(pattern , haha , i + 1))
+            return true;
+    }
+    for (auto haha : curr->get_sami())
+    {
+        if (NFA(pattern , haha , i))
+            return true;
+    }
+    return false;
+
+}
